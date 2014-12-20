adac560a154d0b66aae5ac72c8b00274863b18ee
ki_tom <2240455331@qq.com>
2014-12-20 14:22:05 +0800

    web模板

---
+++ itaste/Apps/Common/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+		'TMPL_PARSE_STRING'=>array(//模板解析
+				'__PCSS__'=> __ROOT__. '/Public/css',
+				'__PJS__'  => __ROOT__. '/Public/js',
+				'__PIMAGES__' =>__ROOT__. '/Public/images'
+		)
+);
